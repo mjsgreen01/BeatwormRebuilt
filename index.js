0b954937aae5445e0e02379d0ccea0587a886cd7
mjsgreen01 <mjsgreen01@gmail.com>
2016-07-02 04:25:00 -0400

    clean up, comments

--- index.js
+++ index.js
@@ -14,72 +14,63 @@ var Nightmare = require('nightmare'),
     nightmare = Nightmare();
 
 var starting_url = 'http://genius.com/artists',
-all_artists_object = {};
+    all_artists_object = {};
 
-
+// get list of urls sorted alphabetically
 function getListUrls() {
-    
+
     nightmare.goto(starting_url)
-        .evaluate(function () {
+        .evaluate(function() {
             var artist_list_urls = [];
             // get the list of hrefs
-            $('a.character_index_list-link').each(function () {
-                artist_list_urls.push( $(this).attr('href') );
+            $('a.character_index_list-link').each(function() {
+                artist_list_urls.push($(this).attr('href'));
             });
 
             return artist_list_urls;
         })
-        // .end()
-        .then(function (list) {
-            // if (urls.length == 0){
-            //     process.exit();
-            // } else {
-                console.log(list);
-                getArtistUrlsAndSave(list);
-            // }
+        .then(function(list) {
+            console.log(list);
+            getArtistUrls(list);
         });
-    
+
 }
 
-function getArtistUrlsAndSave (listUrls) {
+function getArtistUrls(listUrls) {
     url = listUrls.pop();
-    console.log(url);
     // go to a list of artists
     return nightmare.goto(url)
-    .evaluate(function () {
-        var artists_object = {};
-    // save each artists page link as key/value in an object (key=name, val=url)
-        $('a.artists_index_list-artist_name, .artists_index_list>li>a').each(function () {
-            console.log($(this));
-            var artist_name = $(this).text();
-            var artist_url = $(this).attr('href');
-            artists_object[artist_name] = artist_url;
+        .evaluate(function() {
+            var artists_object = {};
+            // save each artists page link as key/value in an object (key=name, val=url)
+            $('a.artists_index_list-artist_name, .artists_index_list>li>a').each(function() {
+                var artist_name = $(this).text();
+                var artist_url = $(this).attr('href');
+                artists_object[artist_name] = artist_url;
+            });
+
+            return artists_object;
+        }).then(function(artists_object) {
+            if (listUrls.length === 0) {
+                // save the entire json-list of artists to a file and exit
+                saveArtistList();
+            } else {
+                // add the artists from this page to the global list
+                _.extend(all_artists_object, artists_object);
+                // go to the next list of artists
+                getArtistUrls(listUrls);
+            }
+
+        }).catch(function(error) {
+            console.error('failed', error);
         });
 
-        return artists_object;
-    }).then(function (artists_object) {
-        if (listUrls.length === 0){
-            // save the entire json-list of artists to a file and exit
-            saveArtistList();
-        } else {
-             // console.log('artists object',artists_object);
-            // add the artists from this page to the global list
-            _.extend(all_artists_object, artists_object);
-            // console.log(all_artists_object);
-            // go to the next list of artists
-            getArtistUrlsAndSave(listUrls);
-        }
-
-    }).catch(function (error) {
-        console.error('failed', error);
-      });
-
-    // save the object as json to a file
 }
 
-function saveArtistList () {
+function saveArtistList() {
+    // save the object as json to a file
     jsonfile.writeFileSync('artists.js', all_artists_object);
-    
+    // exit the script
     process.exit();
 }
 
