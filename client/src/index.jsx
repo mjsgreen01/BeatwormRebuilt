2a794d05c0b72ddd62c87daae25c934f26b753d9
mjsgreen01 <mjsgreen01@gmail.com>
2018-11-10 13:22:54 -0500

    create react entry-file

---
+++ client/src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import { LandingPage } from './landing-page';
+
+function bootStrap() {
+    try {
+        const App = () => (
+            <Router>
+                <Route exact path="/" component={LandingPage}/>
+            </Router>
+        );
+        render(<App/>, document.getElementById('root'));
+
+    } catch (error) {
+        console.error(error);
+        const {status} = error.response;
+        if (status >= 500) {
+            console.log(error)
+            // const Error = <Error50x code={e.status} text={e.statusText} />;
+            // ReactDOM.render(Error, document.getElementById('root'));
+        }
+        console.error("Bootstrap failed:", error);
+    }
+}
+
+bootStrap();
---
+++ client/src/landing-page/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>WELCOME TO BEATWORM</div>
+    );
+};
+
+export { LandingPage };
