db2206d4f6c3137d94a84c83d8c6bdcc90825eda
mjsgreen01 <mjsgreen01@gmail.com>
2019-09-05 21:01:59 -0400

    artist store files

--- client/src/artist-collabs/index.ts
+++ client/src/artist-collabs/index.ts
@@ -1,15 +1,1 @@
-import React from 'react';
-
-// List artist names with links to individual artist pages
-//TODO: add link to artist page
-const ArtistCollab = (props) => {
-    const {
-        artists,
-    } = props;
-
-    return (
-
-    );
-};
-
-export { ArtistCollab };
+export { ArtistCollab } from './artist-collab';
--- client/src/artist-list/artist-list.tsx
+++ client/src/artist-list/artist-list.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
+
 // List artist names with links to individual artist pages
 //TODO: add link to artist page
-const ArtistList = (props) => {
-    const {
-        artists,
-    } = props;
 
-    return (
-        <div className="artist-list">
-            {artists.map(artist => (
-                <div className="artist-list__artist">
-                    {artist.name}
-                </div>
-            ))}
-        </div>
-    );
-};
+class ArtistList extends React.Component {
+
+    componentDidMount() {
+        const {
+            getArtists,
+        } = props;
+
+        //Load list of artists
+        getArtists();
+    }
+
+    render() {
+        const {
+            artists,
+        } = this.props;
+
+        return (
+            <div className="artist-list">
+                {artists.map(artist => (
+                    <div className="artist-list__artist">
+                        {artist.name}
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
 
 export { ArtistList };
